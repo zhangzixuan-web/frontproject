007e7e17ab35186c650a22ac8b5d5d806267d97e
ZhangZixuan <1397188085@qq.com>
2025-07-20 13:56:51 +0800

    新建vue项目

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <RouterView />
+</template>
+
+<script setup lang="js">
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+</style>
---
+++ vue-project/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard-container">
+      <h1>我的生活仪表盘 (Dashboard)</h1>
+      <p>这里将是我们所有酷炫模块的家！</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  // 这里是我们的JS/TS逻辑区，现在先留空
+  </script>
+  
+  <style scoped>
+  .dashboard-container {
+    padding: 20px;
+    background-color: #f0f2f5; /* 一个浅灰色的背景，方便我们看清边界 */
+    min-height: 100vh; /* 让页面至少有屏幕那么高 */
+  }
+  </style>
